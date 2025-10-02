71c84d19a1591f0e36195eec048f7fd3fe86298a
moablex <moablex.ablantedaniel888@gmail.com>
2025-10-02 17:45:13 +0300

    Ref: Editing task class and Detail page

--- lib/features/todo/presentation/screens/task_detail.dart
+++ lib/features/todo/presentation/screens/task_detail.dart
@@ -13,18 +13,50 @@ class _TaskDetailState extends State<TaskDetail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: Column(
         children: [
           SizedBox(height: 20),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Text('${widget.task.title}'),
-              SizedBox(height: 10),
-              Expanded(child: Text('${widget.task.description}')),
-              SizedBox(width: 10),
-              Chip(label: Text('Completed')),
+              Text(
+                '${widget.task.title}',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              Chip(label: Text('50 % Progress')),
             ],
           ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    '${widget.task.description}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                SizedBox(width: 10),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                'Sub Tasks',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.add_task_sharp),
+                color: Colors.blueAccent,
+                iconSize: 30,
+              ),
+            ],
+          ),
+          if (widget.task.subTasks.isEmpty) Center(child: Text('No Sub Tasks')),
         ],
       ),
     );
--- lib/features/todo/presentation/widgets/task_list.dart
+++ lib/features/todo/presentation/widgets/task_list.dart
@@ -52,18 +52,60 @@ class _TaskListState extends State<TaskList> {
   }
 }
 
-class Task {
+// Sub Task model that defines the sub task object
+class SubTask {
   final String id;
   String title;
   String description;
   bool isCompleted;
-
-  Task({
+  SubTask({
     required this.id,
     required this.title,
     required this.description,
     this.isCompleted = false,
   });
+  void toggleCompleted() {
+    isCompleted = !isCompleted;
+  }
+}
+
+// Task modle that defines the task object
+class Task {
+  final String id;
+  String title;
+  String description;
+  bool isCompleted;
+  List<String> tags;
+  DateTime startTime = DateTime.now();
+  DateTime? endTime;
+  double progress;
+
+  List<SubTask> subTasks;
+  DateTime? reminderTime;
+  Task({
+    required this.id,
+    required this.title,
+    required this.description,
+    this.isCompleted = false,
+    this.tags = const [],
+    this.subTasks = const [],
+    this.progress = 0.0,
+    this.reminderTime,
+  });
+
+  void toggleCompleted() {
+    isCompleted = !isCompleted;
+    for (var subTask in subTasks) {
+      subTask.toggleCompleted();
+    }
+  }
+
+  double calculateProgress() {
+    if (subTasks.isEmpty) return isCompleted ? 1.0 : 0.0;
+    int completedTasks =
+        subTasks.where((task) => task.isCompleted == true).length;
+    return completedTasks / subTasks.length;
+  }
 }
 
 class TaskTile extends StatelessWidget {
