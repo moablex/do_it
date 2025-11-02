f3ab13e215e6d258d694b68f0feba338d470cf60
moablex <moablex.ablantedaniel888@gmail.com>
2025-11-02 21:15:39 +0300

    Feat: adding a data layer with isar DB

--- lib/features/todo/domain/repository/todo_repository.dart
+++ lib/features/todo/domain/repository/todo_repository.dart
@@ -1,5 +1,22 @@
 /*
 To Do Repository
-
 */
-abstract class TodoRepository {}
+import 'package:do_it/features/todo/domain/models/todo.dart';
+
+abstract class TodoRepository {
+  //get list of tasks
+
+  Future<List<Task>> getTask();
+
+  //create new task
+
+  Future<void> addTask();
+
+  //Update the existing task
+
+  Future<void> updateTask();
+
+  //Delete task
+
+  Future<void> deleteTask();
+}
